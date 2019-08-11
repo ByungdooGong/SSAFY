097d6e53d8aee00a6e1a5fb053fd25f5e4e4ce07
ByungdooGong <gbd4789@naver.com>
2019-08-11 19:11:06 +0900

    web project1

---
+++ webmobile-sub1-master/src/components/Portfolio.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-hover>
+	<v-card @click.native="detail"
+		slot-scope="{ hover }"
+      :class="`elevation-${hover ? 12 : 2}`"
+      class="mx-auto"
+      >
+		<v-img :src="imgSrc" height="200px">
+		</v-img>
+		<v-card-title primary-title>
+		<div>
+			<div class="headline head">{{title}}</div>
+			<span class="grey--text content">{{body}}</span>
+		</div>
+		</v-card-title>
+	</v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+	name: 'Portfolio',
+	props: {
+		date: {type: String},
+		title: {type: String},
+		body: {type: String},
+		imgSrc: {type: String},
+	},
+	data() {
+		return {
+			
+		}
+	},
+	methods:{
+		detail(){
+			location.href="../portfolio"
+		}
+	}
+}
+</script>
+
+<style>
+.head{
+   white-space: nowrap;
+   overflow: hidden;
+   text-overflow: ellipsis;
+ }
+ .content{
+   display: -webkit-box;
+   -webkit-line-clamp: 3;
+   -webkit-box-orient: vertical;
+   overflow: hidden;
+ }
+</style>
